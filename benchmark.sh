bd8d06a08cfde047895d5fe72e4bc4fa5204f7cf
Kyle Wells <47221196+kqwq@users.noreply.github.com>
2023-04-17 17:12:50 -0400

    formatted

--- benchmark.sh
+++ benchmark.sh
@@ -6,14 +6,14 @@ filename=$(ls images)
 # Loop through each file in the /images directory
 for file in $filename; do
   echo "Running SqueezeNet 1.0 on $file"
-  python3 run_model.py -i $file -m squeezenet1_0
+  python3 run_model.py -i images/$file -m squeezenet1_0
 
   echo "Running SqueezeNet 1.1 on $file"
-  python3 run_model.py -i $file -m squeezenet1_1
+  python3 run_model.py -i images/$file -m squeezenet1_1
 
   echo "Running ResNet 18 on $file"
-  python3 run_model.py -i $file -m resnet18
+  python3 run_model.py -i images/$file -m resnet18
 
   echo "Running AlexNet on $file"
-  python3 run_model.py -i $file -m alexnet
+  python3 run_model.py -i images/$file -m alexnet
 done
--- run_model.py
+++ run_model.py
@@ -4,10 +4,25 @@ from PIL import Image
 from torchvision import transforms
 import torch
 import warnings
+from torch.profiler import profile, record_function, ProfilerActivity
+import time
+
+# Disable PyTorch warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
 
 def main(argv):
+
+    printList = []
+
+    def printLater(*args, **kwargs):
+        printList.append((args, kwargs))
+
+    def printAll():
+        for args, kwargs in printList:
+            print(*args, **kwargs)
+
+    time_start = time.time()
     input_file = ''
     model_name = ''
     help_msg = 'python script.py -i <inputfile> -m <modelname>'
@@ -29,6 +44,10 @@ def main(argv):
         print(help_msg)
         sys.exit(2)
 
+    printLater("================ Model Inference ================")
+    printLater("- Model: ", model_name)
+    printLater("- Input file: ", input_file)
+    printLater()
     model = torch.hub.load('pytorch/vision:v0.10.0',
                            model_name, pretrained=True, verbose=False)
     model.eval()
@@ -45,12 +64,26 @@ def main(argv):
     input_batch = input_tensor.unsqueeze(0)
 
     if torch.cuda.is_available():
+        printLater("Using GPU")
         input_batch = input_batch.to('cuda')
         model.to('cuda')
 
-    with torch.no_grad():
-        output = model(input_batch)
+    # Record the memory usage
+    with profile(activities=[ProfilerActivity.CPU], profile_memory=True) as prof:
+        with record_function("model_inference"):
+            with torch.no_grad():
+                output = model(input_batch)
+    profileOutput = prof.key_averages().table(
+        row_limit=1)
+    row3 = profileOutput.split("\n")[3]
+    memoryUsage = row3[117:117+14].strip()
 
+    # # Record CPU time
+    # row5 = profileOutput.split("\n")[5]
+    # cpuTime = row5[20:].strip()
+    # print("CPU time: ", cpuTime)
+
+    # Print top 5 predictions
     probabilities = torch.nn.functional.softmax(output[0], dim=0)
 
     with open("imagenet_classes.txt", "r") as f:
@@ -58,8 +91,19 @@ def main(argv):
 
     top5_prob, top5_catid = torch.topk(probabilities, 5)
 
+    printLater("#".ljust(5), "Label".ljust(25), "Confidence".ljust(15))
+    printLater("-" * 50)
     for i in range(top5_prob.size(0)):
-        print(categories[top5_catid[i]], top5_prob[i].item())
+        printLater(str(i+1).ljust(5), categories[top5_catid[i]].ljust(25),
+                   str(round(top5_prob[i].item(), 4)).ljust(15))
+
+    time_end = time.time()
+    timeDiffMs = (time_end - time_start) * 1000
+    printLater("Memory usage: ", memoryUsage)
+    printLater("Interface time: ", round(timeDiffMs, 2), "ms")
+    printLater()
+    printLater()
+    printAll()
 
 
 if __name__ == "__main__":
