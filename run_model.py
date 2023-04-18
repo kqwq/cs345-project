f4f57283871e9fc7e6b2bfd76a7fe43bd3f8c09e
Kyle Wells <47221196+kqwq@users.noreply.github.com>
2023-04-18 10:50:10 -0400

    clean up

--- run_model.py
+++ run_model.py
@@ -13,6 +13,7 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 def main(argv):
 
+    # Store print output in a list as to not waste CPU time printing to console
     printList = []
 
     def printLater(*args, **kwargs):
@@ -28,6 +29,8 @@ def main(argv):
                 print(*args, **kwargs, file=f)
 
     time_start = time.time()
+
+    # Command line arguments
     input_file = ''
     model_name = ''
     help_msg = 'python script.py -i <inputfile> -m <modelname>'
@@ -53,10 +56,13 @@ def main(argv):
     printLater("- Model: ", model_name)
     printLater("- Input file: ", input_file)
     printLater()
+
+    # Load the model
     model = torch.hub.load('pytorch/vision:v0.10.0',
                            model_name, pretrained=True, verbose=False)
     model.eval()
 
+    # Load the input image
     input_image = Image.open(input_file)
     preprocess = transforms.Compose([
         transforms.Resize(256),
@@ -65,10 +71,10 @@ def main(argv):
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225]),
     ])
-
     input_tensor = preprocess(input_image)
     input_batch = input_tensor.unsqueeze(0)
 
+    # Move the input and model to GPU for speed if available
     if torch.cuda.is_available():
         printLater("Using GPU")
         input_batch = input_batch.to('cuda')
@@ -78,31 +84,24 @@ def main(argv):
     with profile(activities=[ProfilerActivity.CPU], profile_memory=True) as prof:
         with record_function("model_inference"):
             with torch.no_grad():
-                output = model(input_batch)
+                output = model(input_batch)  # Inference step
     profileOutput = prof.key_averages().table(
         sort_by="self_cpu_memory_usage", row_limit=1)
     row3 = profileOutput.split("\n")[3]
     memoryUsage = row3[117:117+14].strip()
 
-    # # Record CPU time
-    # row5 = profileOutput.split("\n")[5]
-    # cpuTime = row5[20:].strip()
-    # print("CPU time: ", cpuTime)
-
     # Print top 5 predictions
     probabilities = torch.nn.functional.softmax(output[0], dim=0)
-
     with open("imagenet_classes.txt", "r") as f:
         categories = [s.strip() for s in f.readlines()]
-
     top5_prob, top5_catid = torch.topk(probabilities, 5)
 
+    # Print the results
     printLater("#".ljust(5), "Label".ljust(25), "Confidence".ljust(15))
     printLater("-" * 50)
     for i in range(top5_prob.size(0)):
         printLater(str(i+1).ljust(5), categories[top5_catid[i]].ljust(25),
                    str(round(top5_prob[i].item(), 4)).ljust(15))
-
     time_end = time.time()
     timeDiffMs = (time_end - time_start) * 1000
     printLater("Memory usage: ", memoryUsage)
